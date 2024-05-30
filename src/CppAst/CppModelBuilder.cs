f2bd2b332ecd82f747477cefbde27bf4a76d9e3d
Xkein <739502808@qq.com>
2024-05-30 23:16:38 +0800

    user custom parse cursor

--- src/CppAst/CppModelBuilder.cs
+++ src/CppAst/CppModelBuilder.cs
@@ -256,6 +256,12 @@ namespace CppAst
             {
                 _containers.Add(typeKey, containerContext);
             }
+
+            if (symbol is CppElement)
+            {
+                UserCustom.UserParseElement(cursor, CXCursor.Null, symbol as CppElement);
+            }
+
             return containerContext;
         }
 
@@ -466,6 +472,8 @@ namespace CppAst
             {
                 TryToConvertAttributesToMetaAttributes(container);
             }
+
+            UserCustom.UserParseElement(cursor, parent, element);
 
             return CXChildVisitResult.CXChildVisit_Continue;
         }
